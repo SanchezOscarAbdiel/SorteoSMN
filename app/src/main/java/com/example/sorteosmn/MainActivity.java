3bb3bd514a2f7647ff96957ab27bf42edaa25c12
SanchezOscarAbdiel <sanchezoscarabdiel@gmail.com>
2022-03-19 23:11:34 -0600

    prueba

--- app/src/main/java/com/example/sorteosmn/MainActivity.java
+++ app/src/main/java/com/example/sorteosmn/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+ //hola prueba
 
     }
     public void pasaV3 (View view) {
