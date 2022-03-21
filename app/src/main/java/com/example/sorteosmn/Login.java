538d0ae5e10e4ef7f18d1b74794b383482f98f03
SanchezOscarAbdiel <sanchezoscarabdiel@gmail.com>
2022-03-21 17:58:12 -0600

    Intento de conexion2

--- app/src/main/java/com/example/sorteosmn/Login.java
+++ app/src/main/java/com/example/sorteosmn/Login.java
@@ -1,6 +1,6 @@
 package com.example.sorteosmn;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -8,53 +8,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import com.android.volley.RequestQueue;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Login extends AppCompatActivity {
-//comentario random
+public class Login extends AppCompatActivity{
 
- Button show;
-    TextView text;
+//------------------------------------------------------------------------------
+    RequestQueue requestQueue;
+
+    private static final String url = "http://localhost:3306/android.save.php";
+// ------------------------------------------------------------------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        text = (TextView) findViewById(R.id.tvConexion);
-
-        String error="";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyecto_smn_isw", "root", "12345678");
-
-            Statement statement = connection.createStatement();
-
-            text.setText("Conexion: Establecida");
-
-        }
-        catch(Exception e) { error = e.toString();  text.setText("Conexion Fallidad"+error);}
-
 
     }
 
-    class Async extends AsyncTask<Void, Void, Void> {
-        String error="";
-        @Override
-
-        protected Void doInBackground(Void... voids) {
-
-
-            return null;
-        }
-
-//------------------------------------------------------------------------------------
-
-
-    }
 
     //========================================================================
     public void pasaV2 (View view) {
--- app/src/main/java/com/example/sorteosmn/Registro.java
+++ app/src/main/java/com/example/sorteosmn/Registro.java
@@ -1,18 +1,70 @@
 package com.example.sorteosmn;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
+
+    //EditText ;
+
+    //------------------------------------------------------------------------------
+    RequestQueue requestQueue;
+    private static final String url = "http://localhost:3306/android.save.php";
+// ------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        requestQueue = Volley.newRequestQueue(this);
     }
+
+    private void createUser(final String name){
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Registro.this, "correct", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("", name);//variables min 18
+                return params;
+            }
+        };
+
+    }
+
     public void pasaV6 (View view) {
         Intent v1 =new Intent(this,sorteo.class);
         startActivity(v1);
