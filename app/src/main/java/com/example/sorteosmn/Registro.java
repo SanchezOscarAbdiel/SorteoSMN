45b2f2da0e5fc12a340fb70385b5d38d59d3b9c7
SanchezOscarAbdiel <sanchezoscarabdiel@gmail.com>
2022-03-22 23:37:39 -0600

    Spinner colonia

--- app/src/main/java/com/example/sorteosmn/Registro.java
+++ app/src/main/java/com/example/sorteosmn/Registro.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -16,26 +18,82 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 public class Registro extends AppCompatActivity {
 
-    //EditText ;
+
 
     //------------------------------------------------------------------------------
     RequestQueue requestQueue;
     private static final String url = "http://localhost:3306/android.save.php";
 // ------------------------------------------------------------------------------
-
+    private Spinner spinner;
+// ------------------------------------------------------------------------------
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
         requestQueue = Volley.newRequestQueue(this);
+        Spinner spinner = (Spinner) findViewById(R.id.spColonia);
+
+        List<String> spin = new ArrayList<String>();
+        String linea = null;
+
+        InputStream is =this.getResources().openRawResource(R.raw.colonias);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(is));
+        if (is!=null){
+            while (true){
+                try {
+                    if (!((linea=reader.readLine())!=null)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                spin.add(linea.split(",")[0]);
+            }
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String datos[] = spin.toArray(new String[spin.size()]);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, datos);
+        spinner.setAdapter(adapter);
+
     }
 
+    // ------------------------------------------------------------------------------
+    public void llenaSpin() throws IOException{
+
+        List<String> spin = new ArrayList<String>();
+        String linea;
+
+        InputStream is =this.getResources().openRawResource(R.raw.colonias);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(is));
+        if (is!=null){
+            while ((linea=reader.readLine())!=null){
+                spin.add(linea.split("")[0]);
+            }
+        }
+        is.close();
+
+        String datos[] = spin.toArray(new String[spin.size()]);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, datos);
+        spinner.setAdapter(adapter);
+
+    }
+    // ------------------------------------------------------------------------------
     private void createUser(final String name){
 
         StringRequest stringRequest = new StringRequest(
@@ -64,7 +122,7 @@ public class Registro extends AppCompatActivity {
         };
 
     }
-
+    // ------------------------------------------------------------------------------
     public void pasaV6 (View view) {
         Intent v1 =new Intent(this,sorteo.class);
         startActivity(v1);
